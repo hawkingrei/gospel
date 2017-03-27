6a2ae1c4de36eaf05b9784bfe8bd8f1d263fe6ba
真人 <weizhen.wang@duitang.com>
2017-03-27 14:31:59 +0800

    feat: update link tracker

--- client/client.go
+++ client/client.go
@@ -5,19 +5,30 @@ import (
 	"crypto/sha1"
 	"os"
 	"net"
+	"github.com/zeebo/bencode"
+
 )
 
 func main(){
 	var id [20]byte
-        h := sha1.New()
-	ss := "wasdfadsf"
-        h.Write([]byte(ss))
+ 	h := sha1.New()
+	ss, err := os.Hostname()
+    h.Write([]byte(ss))
 	h.Sum(id[:0:20])
-	fmt.Println(id)
-	addr, err := net.ResolveUDPAddr("udp","router.bittorrent.com:6881")
+	targetid := h.Sum(nil)
+
+	fmt.Println(targetid)
+	addr, err := net.ResolveUDPAddr("udp", "router.bittorrent.com:6881")
 	if err != nil {
 		fmt.Println("Can't resolve address: ", err)
 		os.Exit(1)
+	}
+	d := map[string]interface{}{
+		"t": "aa",
+		"y": "q",
+		"q": "get_peers",
+		"a": map[string]interface{}{"info_hash":"abcdefghij0123456789",
+				"id":"mnopqrstuvwxyz123456"},
 	}
 	conn, err := net.DialUDP("udp", nil, addr)
 	if err != nil {
@@ -25,4 +36,23 @@ func main(){
 		os.Exit(1)
 	}
 	defer conn.Close()
+	data, err := bencode.EncodeBytes(d)
+	fmt.Println(string(data))
+	_, err = conn.Write(data)
+	if err != nil {
+		fmt.Println("failed:", err)
+		
+	}
+	fmt.Println("ok")
+	buffer := make([]byte, 1024)
+	conn.Read(buffer)
+	fmt.Println(string(buffer))
+	fmt.Println("===================================")
+	var torrent interface{}
+	err = bencode.DecodeBytes(buffer,&torrent)
+	if err != nil {
+                fmt.Println("failed:", err)
+
+        }
+	fmt.Println(torrent)
 }
