cf0f7e591ce72ba03d07a3a3b4f620f41f700118
真人 <weizhen.wang@duitang.com>
2017-03-30 16:56:18 +0800

    feat: updte client

--- client/client.go
+++ client/client.go
@@ -3,16 +3,96 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"math"
 	"net"
+	"github.com/zeebo/bencode"
 	/*
 	"crypto/sha1"
 	"os"
 	"net"
-	"github.com/zeebo/bencode"
+	
 	*/
 )
 
+func encodeMessage(msg interface{}) ([]byte, error) {
+	return bencode.EncodeBytes(msg)
+}
+
+var (
+	tidVals = map[string]string{
+		"ping": "pn", "find_node": "fn", "get_peers": "gp", "announce_peer": "ap",
+	}
+	tidFuns = map[string]string{
+		"pn": "ping", "fn": "find_node", "gp": "get_peers", "ap": "announce_peer",
+	}
+)
+
+
+func encodeTID(q string, id int16) (tid []byte) {
+	if val, ok := tidVals[q]; ok {
+		uid := uint16(id)
+		if id <= 0 {
+			uid = math.MaxUint16
+		}
+		tid = make([]byte, 4)
+		copy(tid[:2], val[:2])
+		tid[2] = byte(uid & 0xFF00 >> 8)
+		tid[3] = byte(uid & 0x00FF)
+	}
+	return
+}
+
+type kadQueryMessage struct {
+	T []byte                 `bencode:"t"`
+	Y string                 `bencode:"y"`
+	Q string                 `bencode:"q"`
+	A map[string]interface{} `bencode:"a"`
+}
+
+type kadReplyMessage struct {
+	T []byte                 `bencode:"t"`
+	Y string                 `bencode:"y"`
+	R map[string]interface{} `bencode:"r"`
+}
+
+func newQueryMessage(tid []byte, q string, data map[string]interface{}) *kadQueryMessage {
+	return &kadQueryMessage{tid, "q", q, data}
+}
+
+func FindNodeFromAddr(id [20]byte, addr *net.UDPAddr,conn *net.UDPConn) error {
+	data := map[string]interface{}{
+		"id":     id[:],
+		"target": id[:],
+	}
+	return queryMessage("find_node", 0, addr, data,conn)
+}
+
+func sendMessage(addr *net.UDPAddr, data []byte,conn     *net.UDPConn) (err error) {
+	for n, nn := 0, 0; nn < len(data); nn += n {
+		n, err = conn.WriteToUDP(data[nn:], addr)
+		if err != nil {
+			break
+		}
+	}
+	return
+}
+
+func queryMessage(q string, no int16, addr *net.UDPAddr, data map[string]interface{},conn     *net.UDPConn) (err error) {
+	msg := newQueryMessage(encodeTID(q, no), q, data)
+	if b, err := encodeMessage(msg); err == nil {
+		err = sendMessage(addr, b,conn)
+	}
+	return
+}
+
+
+
 func main(){
+
+	conn, err := net.ListenPacket("udp", ":0")
+	if err != nil {
+		return
+	}
 	id := new([20]byte);
 	rand.Read(id[:])
 	fmt.Println(id)
@@ -24,52 +104,12 @@ func main(){
 	}
 	for _, addr := range routers {
 		addr, err := net.ResolveUDPAddr("udp", addr)
+		if err != nil {
+			break
+		}
+		err = FindNodeFromAddr(*id, addr,conn.(*net.UDPConn))
+		if err != nil {
+			break
+		}
 	}
-	/*
-	var id [20]byte
- 	h := sha1.New()
-	ss, err := os.Hostname()
-    h.Write([]byte(ss))
-	h.Sum(id[:0:20])
-	targetid := h.Sum(nil)
-
-	fmt.Println(targetid)
-	addr, err := net.ResolveUDPAddr("udp", "router.bittorrent.com:6881")
-	if err != nil {
-		fmt.Println("Can't resolve address: ", err)
-		os.Exit(1)
-	}
-	d := map[string]interface{}{
-		"t": "aa",
-		"y": "q",
-		"q": "get_peers",
-		"a": map[string]interface{}{"info_hash":"abcdefghij0123456789",
-				"id":"mnopqrstuvwxyz123456"},
-	}
-	conn, err := net.DialUDP("udp", nil, addr)
-	if err != nil {
-		fmt.Println("Can't dial: ", err)
-		os.Exit(1)
-	}
-	defer conn.Close()
-	data, err := bencode.EncodeBytes(d)
-	fmt.Println(string(data))
-	_, err = conn.Write(data)
-	if err != nil {
-		fmt.Println("failed:", err)
-		
-	}
-	fmt.Println("ok")
-	buffer := make([]byte, 10240)
-	conn.Read(buffer)
-	fmt.Printf("%q\n", string(buffer[:]))
-	fmt.Println("===================================")
-	var torrent map[interface{}]
-	err = bencode.DecodeBytes(buffer,&torrent)
-	if err != nil {
-                fmt.Println("failed:", err)
-
-        }
-	fmt.Println(torrent)
-	*/
 }
